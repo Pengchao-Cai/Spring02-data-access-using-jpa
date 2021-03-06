8ed4eff4be9e49092bfcef478da1eba0d7c8f0f8
Pengchao Cai <pengchaocai@gmail.com>
2021-03-06 14:37:23 -0600

    Spring Data Demo

---
+++ src/test/java/com/example/accessDataUsingJpa/AccessDataUsingJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.accessDataUsingJpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccessDataUsingJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
